9fca7009750593925ac4efa7f7afd485a07d14bb
uge2024 <uge.gobernacion@gmail.com>
2025-02-25 17:13:58 -0400

    Update 2024_11_01_003444_create_notifications_table.php

--- database/migrations/2024_11_01_003444_create_notifications_table.php
+++ database/migrations/2024_11_01_003444_create_notifications_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger('user_id');
             $table->string('message');
+            $table->string('type')->nullable();
             $table->boolean('read')->default(false);
             $table->timestamps();
 
